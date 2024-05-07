ab817bc1539912fa74cfb8402eb996f211b351d6
Misha-Boyko <mboykombo@gmail.com>
2024-05-06 19:39:48 -0700

    first commit

---
+++ alarm.sv
@@ -0,0 +1,19 @@
+// CSE140 lab 2  
+// How does this work? How long does the alarm stay on? 
+// (buzz is the alarm itself)
+module alarm(
+  input[6:0]   tmin,
+               amin,
+			   thrs,
+			   ahrs,						 
+  output logic buzz
+);
+
+  always_comb begin
+    //buzz = 0;
+    /* fill in the guts:
+	buzz = 1 when tmin and thrs match amin and ahrs, respectively */
+    if (tmin == amin && thrs == ahrs) buzz = 1;
+    else buzz = 0;
+  end
+endmodule
